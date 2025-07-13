a3e2f0a8a1b7b3155aee9c5e1780973752a0c209
Navkiran Kaur <navkirank45@gmail.com>
2025-07-14 01:28:34 +0530

    Query Selector

---
+++ Day8.js
@@ -0,0 +1,15 @@
+// DOM in depth
+
+// Worked in Console
+
+// Camel Casing is Important in JavaScript
+// document.getElementById('firstElement'); // returns a single element 
+// document.getElementsByClassName('className'); // returns a collection of elements
+// document.querySelector('.className'); // returns the first element that matches the selector
+// document.getFirstElement
+// document.getElementById('firstElement');
+// document.getElementsByClassName('className');
+// document.querySelector('.className');
+
+// query selector is more powerful than getElementById and getElementsByClassName
+// document.querySelectorAll('.className'); // returns a NodeList
